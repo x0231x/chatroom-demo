4867ee81f11b71110acaa3d6766d88f6e5728df6
x0231x <g534894123@gmail.com>
2025-06-29 22:24:53 +0800

    修改前端頁面+新增判斷是否為自己的訊息去改變訊息擺放位置與訊息背景顏色

--- client/chat-ui/src/App.jsx
+++ client/chat-ui/src/App.jsx
@@ -7,45 +7,59 @@ export default function App() {
     if (cached) return cached;
 
     const username = prompt("請輸入使用者名稱")?.trim() || "Anonymous";
-    localStorage.setItem('username', username); // 不關閉嚴格模式時他會mount兩次，我先在這裡就寫入localStorage讓第二次mount時不會再次跳出prompt
-    return username ;
+    localStorage.setItem("username", username); // 不關閉嚴格模式時他會mount兩次，我先在這裡就寫入localStorage讓第二次mount時不會再次跳出prompt
+    return username;
   });
 
   //只要 user 改變就同步到 localStorage
   useEffect(() => {
-    localStorage.setItem('username', user);
+    localStorage.setItem("username", user);
   }, [user]);
-
-
 
   const [text, setText] = useState("");
   const { messages, send } = useChat();
+  const username = localStorage.getItem('username');
 
   return (
-    <main className="flex flex-col max-w-lg mx-auto p-4">
-      <ul className="flex-1 overflow-y-auto mb-4">
-        {messages.map((message, i) => (
-          <li key={i} className="mb-2">
-            <b className="text-primary-600">{message.user}：</b>
-            {message.message}
-          </li>
-        ))}
-      </ul>
+    <>
+      <main className="flex flex-col mx-auto p-4 justify-center items-center w-full">
+        <ul className="flex flex-col overflow-y-auto mb-4 border rounded border-black dark:border-white w-full p-4">
+          {messages.map((m, i) => {
+            const isMe = m.user === username; // 判斷自己
 
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          send(user, text);
-          setText("");
-        }}
-      >
-        <input
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          className="border rounded w-full p-2"
-          placeholder="Type..."
-        />
-      </form>
-    </main>
+            return (
+              <li
+                key={i}
+                className={
+                  [
+                    "mb-2 text-black   p-4 rounded-xl shadow max-w-[60%]",
+                    isMe ? "bg-blue-400 self-end" : "bg-gray-200 dark:bg-amber-50 self-start",
+                  ].join(" ")
+                }
+              >
+                <b className="text-primary-600">{m.user}：</b>
+                {m.message}
+              </li>
+            );
+          })}
+        </ul>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            send(user, text);
+            setText("");
+          }}
+          className="w-full p-8"
+        >
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="border rounded w-full p-2"
+            placeholder="Type..."
+          />
+        </form>
+      </main>
+    </>
   );
 }
